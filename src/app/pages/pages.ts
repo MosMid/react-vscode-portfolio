775c50cb267e542b1ad300f5e75b70bc6fc6f9f7
MosMid <bensegueni.mehdi@gmail.com>
2023-08-17 11:53:05 +0200

    pages update

--- src/app/pages/pages.ts
+++ src/app/pages/pages.ts
@@ -3,7 +3,7 @@ export const pages = [
   { index: 1, name: 'Skills.md', route: '/skills', visible: true },
   { index: 2, name: 'Experience.md', route: '/experience', visible: true },
   { index: 3, name: 'Education.md', route: '/education', visible: true },
-  { index: 4, name: 'projects.md', route: '/projects', visible: true },
+  { index: 4, name: 'Projects.md', route: '/projects', visible: true },
   { index: 5, name: 'Languages.md', route: '/languages', visible: true },
   //{ index: 5, name: 'certificates.md', route: '/certificates', visible: true },
   //{ index: 6, name: 'accomplishments.md', route: '/accomplishments', visible: true },  
