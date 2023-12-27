bf8412bcf79f1d701e9796c2730ce8ae1c0890fb
MosMid <105169046+MosMid@users.noreply.github.com>
2023-12-27 13:20:32 +0100

    Update links.tsx

--- src/app/pages/links.tsx
+++ src/app/pages/links.tsx
@@ -14,7 +14,7 @@ export const links = [
   {
     index: 1,
     title: "Find me on LinkedIn",
-    href: "https://fr.linkedin.com/in/abdelhamid-bensegueni-73b69993",
+    href: "https://fr.linkedin.com/in/mehdi-bensegueni-73b69993",
     icon: <FaLinkedin />,
   },
   {
